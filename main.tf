1ebb5661a2ca731667ca940d95610d6a28ae2269
Gokhan Simsek <user@gokhan.local>
2019-12-21 15:20:01 -0300

    web server variable example

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ vpc_security_group_ids=["${aws_security_group.instance.id}"]
 user_data = <<-EOF
 	#!/bin/bash
 	echo "Hello, world">index.html
-	nohup busybox httpd -f -p 8080 &
+	nohup busybox httpd -f -p "${var.server_port}" &
 	EOF
 
 tags ={
@@ -18,17 +18,28 @@ Name="terraform-example"
 
 }
 }
+variable "server_port"{
+
+description="The port the server will use for http requests"
+default=8080
+}
+
+
 resource "aws_security_group" "instance"{
 name="terraform-example-instance"
 
 ingress{
-	from_port=8080
-	to_port=8080
+	from_port="${var.server_port}"
+	to_port="${var.server_port}"
 	protocol="tcp"
 	cidr_blocks=["0.0.0.0/0"]
 }
 }
 
+output "public_ip" {
 
+value="${aws_instance.example.public_ip}"
+
+}
 
 
