ce32530030d92a3e14337d27403b1ef66f4e6c85
Gokhan Simsek <user@gokhan.local>
2019-12-24 11:02:34 -0300

    Load balancer-self healing example

--- main.tf
+++ main.tf
@@ -2,6 +2,61 @@ provider "aws" {
 region="us-east-1"
 }
 
+
+resource "aws_elb" "example"{
+name="terrafrom-asg-example"
+availability_zones=["${data.aws_availability_zones.all.names[0]}"]
+security_groups=["${aws_security_group.elb.id}"]
+
+listener {
+
+
+lb_port=80
+lb_protocol="http"
+instance_port="${var.server_port}"
+instance_protocol="http"
+
+
+}
+
+health_check {
+
+healthy_threshold=2
+unhealthy_threshold=2
+timeout=3
+interval=30
+target="HTTP:${var.server_port}/"
+
+
+
+
+}
+
+
+}
+
+
+resource "aws_security_group" "elb"{
+name="terraform-example-elb"
+
+ingress{
+        from_port=80
+        to_port=80
+        protocol="tcp"
+        cidr_blocks=["0.0.0.0/0"]
+}
+
+egress{
+
+from_port=0
+to_port=0
+protocol="-1"
+cidr_blocks=["0.0.0.0/0"]
+
+
+}
+}
+
 resource "aws_launch_configuration" "example"{
 
 image_id="ami-40d28157"
@@ -46,6 +101,10 @@ resource "aws_autoscaling_group" "example"{
 launch_configuration="${aws_launch_configuration.example.id}"
 availability_zones=["${data.aws_availability_zones.all.names[0]}"]
 
+load_balancers=["${aws_elb.example.name}"]
+health_check_type="ELB"
+
+
 min_size=2
 max_size=10
 
@@ -56,6 +115,12 @@ value="terraform-asg-example"
 propagate_at_launch=true
 
 }
+
+}
+
+output "elb_dns_name"{
+
+value="${aws_elb.example.dns_name}"
 
 
 }
